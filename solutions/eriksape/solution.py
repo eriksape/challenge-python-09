5e3fbe43ab95c375b589335e5e85a8c0c1f1abde
Erik Sanchez <erik.sape@gmail.com>
2020-08-11 07:38:58 -0500

    eriksape - fix indent

--- solutions/eriksape/solution.py
+++ solutions/eriksape/solution.py
@@ -5,24 +5,24 @@ class Solution:
 
     def duplicate_zeros(self, arr: List[int]):
 
-      index = 0
-      zeroes = 0
-      limit = len(arr) - 1
+        index = 0
+        zeroes = 0
+        limit = len(arr) - 1
 
-      while True:
-        if arr[index] == 0:
-          zeroes += 1
-        index += 1
-        if index + zeroes >= limit:
-          break
+        while True:
+            if arr[index] == 0:
+                zeroes += 1
+            index += 1
+            if index + zeroes >= limit:
+                break
 
-      while limit - zeroes >= 0 and zeroes >= 0:
-        arr[limit] = arr[limit - zeroes]
-        if arr[limit] == 0 and limit - zeroes < index:
-          arr[limit - 1] = 0
-          limit -= 1
-          zeroes -= 1
-        limit -= 1
+        while limit - zeroes >= 0 and zeroes >= 0:
+            arr[limit] = arr[limit - zeroes]
+            if arr[limit] == 0 and limit - zeroes < index:
+                arr[limit - 1] = 0
+                limit -= 1
+                zeroes -= 1
+            limit -= 1
 
-      return arr
+        return arr
  
